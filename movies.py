7d63feebc348175b05f0118f210692035b28701c
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-08-19 21:48:46 -0500

    removed some extra comments

--- movies.py
+++ movies.py
@@ -1,36 +1,3 @@
-# import os
-# import psycopg2
-# from flask import Flask, render_template
-# import urlparse
-# from os.path import exists
-# from os import makedirs
-
-# cur = conn.cursor()
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return 'Hello World!'
-
-# @app.route('/contacts')
-# def contacts():
-#     try:
-#         cur.execute("""SELECT name from salesforce.contact""")
-#         rows = cur.fetchall()
-#         response = ''
-#         my_list = []
-#         for row in rows:
-#             my_list.append(row[0])
-
-#         return render_template('template.html',  results=my_list)
-#     except Exception as e:
-#         print e
-#         return []
-
-# if __name__ == '__main__':
-#     app.run()
-
 import os
 import psycopg2
 from os.path import exists
@@ -73,37 +40,6 @@ def movieRouter():
     print(e)
     return []
 
-
-
-# const express = require('express');
-# const pool = require('../modules/pool');
-
-# const router = express.Router();
-
-# router.get('/:id', (req, res) => {
-#     // return all categories
-#     const queryText = `
-#     SELECT
-#       "movies"."title",
-#       "movies"."poster",
-#       "movies"."description",
-#       ARRAY_AGG("name") "genres"
-#     FROM "movies"
-#     JOIN "movie_genres" ON "movie_id" = "movies"."id"
-#     JOIN "genres" ON "genre_id" = "genres"."id"
-#     WHERE "movies"."id" = ${req.params.id}
-#     GROUP BY "movies"."id"
-#     ORDER BY "movies"."title" ASC`;
-#     pool.query(queryText)
-#         .then( (result) => {
-#             res.send(result.rows);
-#         })
-#         .catch( (error) => {
-#             console.log(`Error on query ${error}`);
-#             res.sendStatus(500);
-#         });
-# });
-
 # router.put('/:id', (req, res) => {
 #   // return all genres
 #   const queryText = `UPDATE movies
@@ -127,9 +63,6 @@ def movieRouter():
 
 
 # module.exports = router;
-
-
-
 
 
 @app.route('/details/<id>', methods=['GET'])
