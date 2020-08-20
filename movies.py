420cbb5b053828829b4719f65efbbc955edb149e
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-08-19 21:47:33 -0500

    Got the home page to load, details page too

--- movies.py
+++ movies.py
@@ -33,10 +33,9 @@
 
 import os
 import psycopg2
-# from urllib.parse import urlparse
 from os.path import exists
 from os import makedirs
-from flask import Flask, render_template
+from flask import Flask, jsonify
 app = Flask(__name__)
 # config = {
 #         user: process.env.PG_USER || null, //env var: PGUSER
@@ -48,7 +47,6 @@ app = Flask(__name__)
 #         idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 #     };
 
-# url = urlparse(os.environ.get('DATABASE_URL'))
 db = "dbname=%s host=%s " % ('saga_movies_weekend', 'localhost')
 schema = "schema.sql"
 conn = psycopg2.connect(db)
@@ -57,26 +55,45 @@ cur = conn.cursor()
 
 
 @app.route('/movies', methods=['GET'])
-def movies():
+def movieRouter():
   try:
     cur.execute("""SELECT * FROM movies ORDER BY title ASC""")
     rows = cur.fetchall()
-    response = ''
-    my_list = []
-    for row in rows:
-      my_list.append(row[0])
+    colnames = [desc[0] for desc in cur.description]
+    cur.close()
+    print(colnames)
+    response = []
+    for x in range( 0, len(rows) ):
+      response.append({'id':rows[x][0]})
+      for y in range(1, len(colnames)):
+        response[x].update({colnames[y]:rows[x][y]})
 
-    return render_template('template.html',  results=my_list)
+    return jsonify(response)
   except Exception as e:
     print(e)
     return []
 
 
 
+# const express = require('express');
+# const pool = require('../modules/pool');
 
-# router.get('/', (req, res) => {
+# const router = express.Router();
+
+# router.get('/:id', (req, res) => {
 #     // return all categories
-#     const queryText = `SELECT * FROM movies ORDER BY title ASC`;
+#     const queryText = `
+#     SELECT
+#       "movies"."title",
+#       "movies"."poster",
+#       "movies"."description",
+#       ARRAY_AGG("name") "genres"
+#     FROM "movies"
+#     JOIN "movie_genres" ON "movie_id" = "movies"."id"
+#     JOIN "genres" ON "genre_id" = "genres"."id"
+#     WHERE "movies"."id" = ${req.params.id}
+#     GROUP BY "movies"."id"
+#     ORDER BY "movies"."title" ASC`;
 #     pool.query(queryText)
 #         .then( (result) => {
 #             res.send(result.rows);
@@ -87,13 +104,85 @@ def movies():
 #         });
 # });
 
+# router.put('/:id', (req, res) => {
+#   // return all genres
+#   const queryText = `UPDATE movies
+#                     SET title=$1,
+#                     description=$2
+#                     WHERE ID=$3`;
+#   const queryValues = [
+#     req.body.title,
+#     req.body.description,
+#     req.body.id,
+#   ]
+#   pool.query(queryText, queryValues)
+#       .then( (result) => {
+#           res.sendStatus(200);
+#       })
+#       .catch( (error) => {
+#           console.log(`Error on query ${error}`);
+#           res.sendStatus(500);
+#       });
+# });
 
-@app.route('/<name>')
-def hello_name(name):
-  return "Hello, {}!".format(name)
+
+# module.exports = router;
+
+
+
+
+
+@app.route('/details/<id>', methods=['GET'])
+def detailRouter(id):
+  try:
+    cur.execute('''SELECT
+      "movies"."title",
+      "movies"."poster",
+      "movies"."description",
+      ARRAY_AGG("name") "genres"
+    FROM "movies"
+    JOIN "movies_genres" ON "movie_id" = "movies"."id"
+    JOIN "genres" ON "genre_id" = "genres"."id"
+    WHERE "movies"."id" = {}
+    GROUP BY "movies"."id"
+    ORDER BY "movies"."title" ASC'''.format(id))
+    rows = cur.fetchall()
+    colnames = [desc[0] for desc in cur.description]
+    cur.close()
+    print(colnames)
+    response = []
+    for x in range( 0, len(rows) ):
+      response.append({'id':id })
+      for y in range(0, len(colnames)):
+        response[x].update({colnames[y]:rows[x][y]})
+    print(response)
+    return jsonify(response)
+  except Exception as e:
+    print(e)
+    return []
+
+@app.route('/genres', methods=['GET'])
+def genreRouter():
+  try:
+    cur.execute("""SELECT * FROM genres ORDER BY id ASC""")
+    rows = cur.fetchall()
+    colnames = [desc[0] for desc in cur.description]
+    cur.close()
+    print(colnames)
+    response = []
+    for x in range( 0, len(rows) ):
+      response.append({'id':rows[x][0]})
+      for y in range(1, len(colnames)):
+        response[x].update({colnames[y]:rows[x][y]})
+
+    return jsonify(response)
+  except Exception as e:
+    print(e)
+    return []
+
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
 
 
 # const express = require('express');
